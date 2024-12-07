9b612bd6dd74a5b9183a97b658a36d129a19db15
Spokojny <rbasta100@gmail.com>
2024-12-07 11:40:52 +0100

    #1R update mainpage w xamlu, CollectionView z bindingami ze statycznymi danymi

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,32 @@
-﻿namespace ExpenseView
+﻿using System.Collections.ObjectModel;
+
+namespace ExpenseView
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;// R ustawienie bindingcontext pozwala collectionview wiazac sie z wlasciwosciami, laczy xaml z cs, oznacza to ze wszystkie wlasciowsci jak wydatki beda 'widoczne' w xamlu do bindingu
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public class Wydatek
         {
-            count++;
+            public required string Nazwa { get; set; }
+            public required double Kwota { get; set; }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            public string KwotaZl => $"{Kwota:F2} zł";
+            //R sformatowana kwota wydatku, 2 miejsca po przecinku i dopisane zł na koncu
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        public ObservableCollection<Wydatek> Wydatki { get; set; } = new ObservableCollection<Wydatek>();
+        //R obserrvablecollection sprawia ze jak zmieni sie kolekcja to zmieni sie tez w xamlu
+        private void dodajWydatekBtn_Clicked(object sender, EventArgs e)
+        {
+            //R dodanie nowego wydatku, trzeba jeszcze dodac formularz bo nie ma xdd
+            Wydatki.Add(new Wydatek { Nazwa = "Test", Kwota = 10.00  });
         }
     }
 
