0092b436eea7a2f4984190b1d1d4ad4b63462ea1
Spokojny <rbasta100@gmail.com>
2024-12-07 09:37:13 +0100

    Dodaj pliki projektów.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExpenseView
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
